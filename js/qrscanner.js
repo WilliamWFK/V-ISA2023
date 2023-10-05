bbe026214956f9811792edc5ba25a58ef498795d
Will <42400351+WilliamWFK@users.noreply.github.com>
2023-10-05 15:23:58 +1300

    QR CODE

---
+++ js/qrscanner.js
@@ -0,0 +1,18 @@
+
+function onScanSuccess(decodedText, decodedResult) {
+    // handle the scanned code as you like, for example:
+    const hash = md5(decodedText);
+    console.log(`Code matched = ${decodedText}`, decodedResult);
+  }
+  
+  function onScanFailure(error) {
+    // handle scan failure, usually better to ignore and keep scanning.
+    // for example:
+    console.warn(`Code scan error = ${error}`);
+  }
+  
+  let html5QrcodeScanner = new Html5QrcodeScanner(
+    "reader",
+    { fps: 10, qrbox: {width: 250, height: 250} },
+    /* verbose= */ false);
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
